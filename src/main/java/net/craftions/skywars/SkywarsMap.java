8ded1afda76dc53fcf90e937a837700b337a7677
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-24 21:33:57 +0200

    Update SkywarsMap.java

--- src/main/java/net/craftions/skywars/SkywarsMap.java
+++ src/main/java/net/craftions/skywars/SkywarsMap.java
@@ -1,16 +1,32 @@
 package net.craftions.skywars;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class SkywarsMap {
 
     private final int teams, teamSize;
     private final Location respawn;
+    private final List<Block> chests;
+    private final ChestGenerator gen;
 
     public SkywarsMap(int teams, int teamSize, Location respawn) {
+        this(teams, teamSize, respawn, ChestGenerator.DEFAULT);
+    }
+
+    public SkywarsMap(int teams, int teamSize, Location respawn, ChestGenerator gen) {
         this.teams = teams;
         this.teamSize = teamSize;
         this.respawn = respawn;
+        this.chests = new ArrayList<>();
+        this.gen = gen;
+    }
+
+    public void addChest(Block block) {
+        chests.add(block);
     }
 
     public final int getTeams() {
@@ -26,4 +42,8 @@ public abstract class SkywarsMap {
     }
 
     public abstract Location getSpawnLocation(int team);
+
+    public List<Block> getChests() {
+        return chests;
+    }
 }
