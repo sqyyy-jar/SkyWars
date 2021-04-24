bb6a03d61454917e36ac287e36fa3e74e75215cd
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-24 20:04:53 +0200

    Update EventDeath.java

--- src/main/java/net/craftions/skywars/listeners/EventDeath.java
+++ src/main/java/net/craftions/skywars/listeners/EventDeath.java
@@ -3,6 +3,7 @@ package net.craftions.skywars.listeners;
 import net.craftions.skywars.Game;
 import net.craftions.skywars.Skywars;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -10,13 +11,17 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public class EventDeath implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
+        Player p = e.getEntity();
         Skywars sw = Skywars.getInstance();
         Game game = sw.getCurrentGame();
         if (game == null) {
-            e.getEntity().teleport(sw.getSpawn() == null ? sw.getSpawn() : new Location(e.getEntity().getWorld(), 0, 100, 0));
+            p.teleport(sw.getSpawn() == null ? sw.getSpawn() : new Location(p.getWorld(), 0, 100, 0));
             e.setDeathMessage(null);
-            e.getEntity().setHealth(20);
+            p.setHealth(20);
             return;
+        }
+        if (game.isAlive(p)) {
+            game.kill(p);
         }
     }
 }
