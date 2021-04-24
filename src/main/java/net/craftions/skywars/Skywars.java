231807a2ed30d7c6e5ba7e40f540df0be1fdd529
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-24 19:48:40 +0200

    Create Skywars.java

---
+++ src/main/java/net/craftions/skywars/Skywars.java
@@ -0,0 +1,37 @@
+package net.craftions.skywars;
+
+import org.bukkit.Location;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skywars extends JavaPlugin {
+
+    private static Skywars instance;
+    private Game game;
+    private Location spawn;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        this.spawn = this.getConfig().getLocation("lobby.spawn");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public void createGame(int teams, int teamSize) {
+        this.game = new Game(teams, teamSize);
+    }
+
+    public Game getCurrentGame() {
+        return game;
+    }
+
+    public Location getSpawn() {
+        return this.spawn;
+    }
+
+    public static Skywars getInstance() {
+        return instance;
+    }
+}
