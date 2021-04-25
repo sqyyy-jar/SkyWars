b7f37c2b837cb39c6f622ff2049f724e6fe69707
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-25 12:52:29 +0200

    Update EventJoinQuit.java

--- src/main/java/net/craftions/skywars/listeners/EventJoinQuit.java
+++ src/main/java/net/craftions/skywars/listeners/EventJoinQuit.java
@@ -1,6 +1,9 @@
 package net.craftions.skywars.listeners;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import net.craftions.skywars.Skywars;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,12 +14,28 @@ public class EventJoinQuit implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().teleport(Skywars.getInstance().getSpawn());
         if (Skywars.getInstance().getCurrentGame() == null) {
-            e.getPlayer().sendMessage("");
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF("lobby");
+            e.getPlayer().sendPluginMessage(Skywars.getInstance(), "BungeeCord", out.toByteArray());
+            return;
         }
+        if (Skywars.getInstance().getCurrentGame().isStarted() || Skywars.getInstance().getCurrentGame().isReady()) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF("lobby");
+            e.getPlayer().sendPluginMessage(Skywars.getInstance(), "BungeeCord", out.toByteArray());
+            return;
+        }
+        Skywars.getInstance().getCurrentGame().initPlayer(e.getPlayer());
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
-
+        if (Skywars.getInstance().getCurrentGame() == null) {
+            e.setQuitMessage(null);
+            return;
+        }
+        Skywars.getInstance().getCurrentGame().disconnect(e.getPlayer());
     }
 }
