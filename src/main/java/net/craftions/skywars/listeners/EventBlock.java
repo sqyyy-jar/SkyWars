3c6d6d94496f22a9e8cab0240e552615b2d7b93e
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-25 12:12:19 +0200

    Update EventBlock.java

--- src/main/java/net/craftions/skywars/listeners/EventBlock.java
+++ src/main/java/net/craftions/skywars/listeners/EventBlock.java
@@ -2,9 +2,11 @@ package net.craftions.skywars.listeners;
 
 import net.craftions.skywars.Skywars;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class EventBlock implements Listener {
 
@@ -14,11 +16,24 @@ public class EventBlock implements Listener {
             e.setCancelled(true);
             return;
         }
-        e.setCancelled(Skywars.getInstance().getCurrentGame().isStarted());
+        e.setCancelled(!Skywars.getInstance().getCurrentGame().isStarted());
     }
 
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
+        if (Skywars.getInstance().getCurrentGame() == null) {
+            e.setCancelled(true);
+            return;
+        }
+        e.setCancelled(!Skywars.getInstance().getCurrentGame().isStarted());
+    }
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void on(InventoryClickEvent e) {
+        if (Skywars.getInstance().getCurrentGame() == null) {
+            e.setCancelled(true);
+            return;
+        }
+        e.setCancelled(!Skywars.getInstance().getCurrentGame().isStarted());
     }
 }
