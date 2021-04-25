95c0c61b1879e888d71a24668de852694c7a83e0
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-25 12:17:03 +0200

    Update Skywars.java

--- src/main/java/net/craftions/skywars/Skywars.java
+++ src/main/java/net/craftions/skywars/Skywars.java
@@ -2,6 +2,7 @@ package net.craftions.skywars;
 
 import net.craftions.skywars.listeners.EventBlock;
 import net.craftions.skywars.listeners.EventDeath;
+import net.craftions.skywars.listeners.EventJoinQuit;
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,6 +19,7 @@ public final class Skywars extends JavaPlugin {
         this.spawn = this.getConfig().getLocation("lobby.spawn");
         this.getServer().getPluginManager().registerEvents(new EventBlock(), this);
         this.getServer().getPluginManager().registerEvents(new EventDeath(), this);
+        this.getServer().getPluginManager().registerEvents(new EventJoinQuit(), this);
     }
 
     @Override
